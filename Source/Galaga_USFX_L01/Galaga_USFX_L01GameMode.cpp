cfd6c244e2aaa375d00d0c252cedaee20b00d8ed
Karen . <karenamurriohuaygua@gmail.com>
2024-03-05 22:42:19 -0400

    Agregar archivos de proyecto.

---
+++ Source/Galaga_USFX_L01/Galaga_USFX_L01GameMode.cpp
@@ -0,0 +1,28 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Galaga_USFX_L01GameMode.h"
+#include "Galaga_USFX_L01Pawn.h"
+#include "NaveEnemiga.h"
+
+AGalaga_USFX_L01GameMode::AGalaga_USFX_L01GameMode()
+{
+	// set default pawn class to our character class
+	DefaultPawnClass = AGalaga_USFX_L01Pawn::StaticClass();
+}
+
+void AGalaga_USFX_L01GameMode::BeginPlay()
+{
+
+	Super::BeginPlay();
+
+	FVector ubicacionNave = FVector(-900.0f, 50.0f, 250.0f);
+	FRotator rotacionNave = FRotator(0.0f, 0.0f, 0.0f);
+
+	UWorld* const World = GetWorld();
+	if (World != nullptr)
+	{
+		// spawn the projectile
+		World->SpawnActor<ANaveEnemiga>(ubicacionNave, rotacionNave);
+	}
+}
+
